add wandb to a2c, add gpt2, fix bugs, add normalization

a2c/a2c_agent_torch.py
@@ -5,10 +5,9 @@ import torch
 import torch.optim as optim
 import numpy as np
 import pickle
-import matplotlib.pyplot as plt
+import wandb
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-env = gym.make("CartPole-v0")
 
 def compute_returns(final_value, rewards, masks, gamma=0.99):
     total_reward = final_value
@@ -20,7 +19,6 @@ def compute_returns(final_value, rewards, masks, gamma=0.99):
 
 def train_model(env, model, optimizer, n_iters):
     trajectories = []
-    plt_returns = []
     for episode in range(n_iters):
         log_probs = []
         values = []
@@ -32,13 +30,14 @@ def train_model(env, model, optimizer, n_iters):
         #         pickle.dump(trajectories, dataset_file)
         #     trajectories = []
         # recordTrajectories(env, model, trajectories)
-        state = env.reset()
+        recordTrajectories(env, model, trajectories)
+        state, _info = env.reset()
 
         for step in range(200):
-            state = torch.FloatTensor(state).to(device)
+            state = torch.from_numpy(np.array(state)).to(device)
             dist, value = model(state.unsqueeze(0))
             action = dist.sample()
-            next_state, reward, done, _ = env.step(action.cpu().numpy()[0])
+            next_state, reward, done, _truncated, _info = env.step(action.cpu().numpy()[0])
 
             log_prob = dist.log_prob(action).unsqueeze(0)
             log_probs.append(log_prob)
@@ -50,7 +49,6 @@ def train_model(env, model, optimizer, n_iters):
             
             if done:
                 print(f'Iteration: {episode}, Score: {step}')
-                plt_returns.append(step)
                 break
 
         final_value = 0
@@ -71,22 +69,23 @@ def train_model(env, model, optimizer, n_iters):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        wandb.log({"loss": loss.item(), "return": step})
     torch.save(model, 'a2c_model')
+    with open(f"dataset/trajectories.pkl", "wb") as dataset_file:
+        pickle.dump(trajectories, dataset_file)
     env.close()
-    plt.plot(plt_returns)
-    plt.savefig('returns.png')
 
 def recordTrajectories(env, model, trajectories):
     with torch.no_grad():
-        for _ in range(30):
-            state = env.reset()
+        for _ in range(20):
+            state, _info = env.reset()
             trajectory = {"states": [], "actions": [], "rewards": [], "dones": []}
             for _ in range(200):
-                state = torch.FloatTensor(state).unsqueeze(0).to(device)
+                state = torch.from_numpy(np.array(state)).unsqueeze(0).to(device)
                 trajectory["states"].append(state)
                 dist, _ = model(state)
                 action = dist.sample()
-                state, reward, done, _ = env.step(action.cpu().numpy()[0])
+                state, reward, done, _truncated, _info = env.step(action.cpu().numpy()[0])
                 trajectory["actions"].append(np.array([1 if i == action else 0 for i in range(env.action_space.n)]))
                 trajectory["rewards"].append(reward)
                 trajectory["dones"].append(done)
@@ -94,39 +93,44 @@ def recordTrajectories(env, model, trajectories):
 
 
 
-def testModel(env, model, num_episodes):
+def testModel(model, num_episodes):
+    env = gym.make("CartPole-v1", render_mode='human')
     with torch.no_grad():
-        trajectory = []
-        recordTrajectories(env, model, trajec)
         for _ in range(num_episodes):
-            state = env.reset()
+            state, _info = env.reset()
             for _ in range(200):
-                env.render()
                 state = torch.FloatTensor(state).unsqueeze(0).to(device)
                 dist, _ = model(state)
                 action = dist.sample()
-                state, _reward, _done, _ = env.step(action.cpu().numpy()[0])
+                state, _reward, _done, _truncated, _info = env.step(action.cpu().numpy()[0])
     env.close()
 
 def main():
     if len(sys.argv) != 3:
         print('Please input either test or train and the number of episodes to test')
         return
-
-    state_size = env.observation_space.shape[0]
-    action_size = env.action_space.n
+    
+    wandb.login()
+    wandb.init(project='a2c')
+    wandb.run.name = 'Adam'
 
     num_hidden = 128
     _, run_type, num_episodes = sys.argv
+    print("running on", device)
 
     if run_type == 'test' and os.path.exists('a2c_model'):
         model = torch.load('a2c_model')
     else:
+        env = gym.make("CartPole-v1")
+        state_size = env.observation_space.shape[0]
+        action_size = env.action_space.n
         model = ActorCritic(state_size, action_size, num_hidden).to(device)
-        optimizer = optim.NAdam(model.parameters())
-        train_model(env, model, optimizer, 600)
+        optimizer = optim.Adam(model.parameters())
+        train_model(env, model, optimizer, 700)
+        env.close()
 
-    testModel(env, model, int(num_episodes))
+    # testModel(model, int(num_episodes))
+    wandb.run.finish()
 
 if __name__ == '__main__':
     main()

decisiontransformer/dataset/trajectory_dataset.py
@@ -7,23 +7,26 @@ import os
 
 class TrajectoryDataset(Dataset):
     def __init__(self, base_path, context_len, state_dim, action_dim):
-        self.base_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), base_path)
+        # self.base_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), base_path)
         self.c_len = context_len
         self.state_dim = state_dim
         self.action_dim = action_dim
-        # self.state_mean, self.state_std = self._compute_mean_std()
         self.trajectories = []
-        for i in range(len(os.listdir(self.base_path))):
-            interval = i * 150
-            path = os.path.join(self.base_path, f'traj_{interval}-{interval + 150}.pkl')
-            with open(path, 'rb') as f:
-                file_trajectories = pickle.load(f)
-            self.trajectories.extend(file_trajectories)
-            print(i)
+        # for i in range(len(os.listdir(self.base_path))):
+        #     interval = i * 150
+        #     path = os.path.join(self.base_path, f'traj_{interval}-{interval + 150}.pkl')
+        #     with open(path, 'rb') as f:
+        #         file_trajectories = pickle.load(f)
+        #     self.trajectories.extend(file_trajectories)
+        #     print(i)
+        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'trajectories.pkl')
+        with open(path, 'rb') as f:
+            self.trajectories = pickle.load(f)
         print('done')
+        self.state_mean, self.state_std = self._compute_mean_std()
 
     def __len__(self):
-        return len(os.listdir(self.base_path)) * 150
+        return len(self.trajectories)
 
     def __getitem__(self, idx):
         # interval = (idx // 150) * 150
@@ -37,16 +40,16 @@ class TrajectoryDataset(Dataset):
         returns = self._compute_returns(0, traj["rewards"], mask, gamma=1)
 
         rand_start = random.randint(0, len(traj['states']) - 1)
-        s = np.stack(traj['states'][rand_start:rand_start+self.c_len]).reshape((-1, self.state_dim))
+        s = torch.stack(traj['states'][rand_start:rand_start+self.c_len]).reshape((-1, self.state_dim))
         a = np.stack(traj['actions'][rand_start:rand_start+self.c_len]).reshape((-1, self.action_dim))
         r = np.stack(returns[rand_start:rand_start+self.c_len]).reshape((-1, 1))
         d = np.stack(traj['dones'][rand_start:rand_start+self.c_len])
         timesteps = np.arange(rand_start, rand_start+len(s)) # TODO: check if padding cutoff is necessary and if rtg needs padding
 
         tlen = s.shape[0]
-        s = np.concatenate((np.zeros((self.c_len - tlen, self.state_dim)), s))
-        s = torch.tensor(s)
-        # s = (s - self.state_mean) / self.state_std
+        s = s.cpu()
+        s = torch.concatenate((torch.zeros((self.c_len - tlen, self.state_dim)), s))
+        s = (s - self.state_mean) / self.state_std
         a = np.concatenate((np.ones((self.c_len - tlen, self.action_dim)) * -10., a))
         r = np.concatenate((np.zeros((self.c_len - tlen, 1)), r))
         d = np.concatenate((np.ones(self.c_len - tlen) * 2, d))
@@ -62,3 +65,7 @@ class TrajectoryDataset(Dataset):
             total_reward = gamma * total_reward * masks[step] + rewards[step]
             discounted_rewards[step] = total_reward
         return discounted_rewards
+
+    def _compute_mean_std(self):
+        states = torch.cat([torch.concatenate(traj['states'], axis=0) for traj in self.trajectories], 0)
+        return torch.mean(states, axis=0).cpu(), torch.std(states, axis=0).cpu() + 1e-6

decisiontransformer/decision_transformer.py
@@ -2,24 +2,36 @@ import numpy as np
 import torch
 import torch.nn as nn
 import transformers
-from minGPT.transformer import minGPT
+# from minGPT.transformer import minGPT
+from gpt2 import GPT2Model
 
 class DecisionTransformer(nn.Module):
-    def __init__(self, state_dim, act_dim, hidden_size, context_length, max_ep_len, action_tanh, **kwargs):
+    def __init__(self, state_dim, act_dim, hidden_size, context_length, max_ep_len, action_tanh, device, **kwargs):
         super().__init__()
         self.hidden_size = hidden_size
+        # transformer_config = transformers.GPT2Config(
+        #     vocab_size=act_dim,
+        #     n_embd=hidden_size,
+        #     block_size=context_length * 3,
+        #     **kwargs,
+        # )
         transformer_config = transformers.GPT2Config(
-            vocab_size=act_dim,
+            vocab_size=1,
             n_embd=hidden_size,
-            block_size=context_length * 3,
-            **kwargs,
+            n_ctx=context_length * 3,
+            device=device,
+            **kwargs
         )
 
         self.state_dim = state_dim
         self.act_dim = act_dim
         self.c_len = context_length
+        if device == 'auto':
+            self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        else:
+            self.device = device
 
-        self.transformer = minGPT(transformer_config)
+        self.transformer = GPT2Model(transformer_config)
         self.embed_timestep = nn.Embedding(max_ep_len, hidden_size)
         self.embed_return = nn.Linear(1, hidden_size)
         self.embed_state = nn.Linear(state_dim, hidden_size)
@@ -52,12 +64,18 @@ class DecisionTransformer(nn.Module):
 
         stacked_attn_mask = torch.stack(
             (attn_mask, attn_mask, attn_mask), dim=1
-        ).permute(0, 2, 1).reshape(batch_size, 3*seq_len)
+        ).permute(0, 2, 1).reshape(batch_size, 3*seq_len).to(device=self.device)
 
-        logits = self.transformer(
+        # logits = self.transformer(
+        #     inputs_embeds=stacked_inputs,
+        #     attention_mask=stacked_attn_mask,
+        # )
+
+        outputs = self.transformer(
             inputs_embeds=stacked_inputs,
             attention_mask=stacked_attn_mask,
         )
+        logits = outputs['last_hidden_state']
 
         logits = logits.reshape(batch_size, seq_len, 3, self.hidden_size).permute(0, 2, 1, 3)
         action_hidden = logits[:, 2, :, :]
@@ -100,7 +118,7 @@ class DecisionTransformer(nn.Module):
         action_preds = self.forward(
             states, actions, returns_to_go, timesteps, attn_mask=attention_mask, **kwargs)
 
-        return action_preds[0]
+        return action_preds[0, -1]
 
     def configure_optimizers(self, train_config):
         return self.transformer.configure_optimizers(train_config)

decisiontransformer/training.py
@@ -23,7 +23,16 @@ class Trainer:
 
         self.iter_num = 0
         self.iter_time = 0.0
-        self.optimizer = self.model.configure_optimizers(config)
+        self.optimizer = torch.optim.AdamW(
+            model.parameters(),
+            lr=config["learning_rate"],
+            weight_decay=config['weight_decay']
+        )
+        self.scheduler = torch.optim.lr_scheduler.LambdaLR(
+            self.optimizer,
+            lambda steps: min((steps+1)/config['warmup_steps'], 1)
+        )
+        # self.optimizer = self.model.configure_optimizers(config)
         self.loss_fn = loss_fn
 
     def train(self):
@@ -42,8 +51,8 @@ class Trainer:
                 batch_size=config["batch_size"],
                 num_workers=config["num_workers"],
             )
-            print(epoch)
 
+            batch_losses = []
             for batch_num, sample in enumerate(tqdm(train_loader)):
                 states, actions, returns, dones, timesteps, attn_mask = sample
                 states = states.to(dtype=torch.float, device=self.device)
@@ -62,9 +71,12 @@ class Trainer:
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
-                train_losses.append(loss.detach().cpu().item())
+                if self.scheduler is not None:
+                    self.scheduler.step()
+                batch_losses.append(loss.detach().cpu().item())
                 wandb.log({'loss': loss.item()})
-            print(train_losses[-1])
+            train_losses.extend(batch_losses)
+            print(np.mean(np.asarray(batch_losses)))
         
         self.train_losses = train_losses
         logs['time/training'] = time.time() - train_start
@@ -75,8 +87,8 @@ class Trainer:
         model.to(device=device)
         state_dim, act_dim = env.observation_space.shape[0], env.action_space.n
 
-        state_mean = torch.from_numpy(state_mean).to(device=device)
-        state_std = torch.from_numpy(state_std).to(device=device)
+        # state_mean = torch.from_numpy(state_mean).to(device=device)
+        # state_std = torch.from_numpy(state_std).to(device=device)
 
         state = env.reset()
 
@@ -86,26 +98,28 @@ class Trainer:
         actions = torch.zeros((0, act_dim), device=device, dtype=torch.float32)
         ret_to_go = torch.zeros(1, device=device, dtype=torch.float32)
         ret_to_go[0] = target_return
+        timesteps = torch.from_numpy(np.arange(200)).to(device=device, dtype=torch.float32)
 
         episode_return, episode_length = 0, 0
-        for t in range(200):
+        for t in timesteps:
 
             # add padding
             actions = torch.cat([actions, torch.zeros((1, act_dim), device=device)], dim=0)
-            ret_to_go = torch.cat([ret_to_go, torch.zeros(1, device=device)])
+            norm_state = states.to(dtype=torch.float32) # (states.to(dtype=torch.float32) - state_mean) / state_std
 
             action = model.get_action(
-                (states.to(dtype=torch.float32) - state_mean) / state_std,
+                norm_state,
                 actions.to(dtype=torch.float32),
                 ret_to_go.to(dtype=torch.float32),
-                target_return=target_return,
+                timesteps
             )
             action = action.detach().cpu().numpy()
-
-            state, reward, done, _ = env.step(action)
+            env_action = np.argmax(action)
+            state, reward, done, _ = env.step(env_action)
 
             cur_state = torch.from_numpy(state).to(device=device).reshape(1, state_dim)
             states = torch.cat([states, cur_state], dim=0)
+            ret_to_go = torch.cat([ret_to_go, torch.zeros(1, device=device)])
             ret_to_go[-1] = ret_to_go[-2] - reward
 
             episode_return += reward
@@ -118,30 +132,70 @@ class Trainer:
 
 
 def main():
+    # wandb.login()
+    # env = gym.make('CartPole-v0')
+    # print(torch.cuda.is_available())
+
+    # with wandb.init(project='decision-transformer'):
+    #     wandb.config = {
+    #         "learning_rate": 2e-4,
+    #         "epochs": 1000,
+    #         "batch_size": 64,
+    #         "hidden_size": 128,
+    #         "c_len": 4,
+    #         "device": "auto",
+    #         "weight_decay": 0.01,
+    #         "betas": (0.9, 0.999),
+    #         "num_workers": 2
+    #     }
+    #     config = wandb.config
+    #     env = gym.make('CartPole-v0')
+
+    #     state_dim = env.observation_space.shape[0]
+    #     action_dim = env.action_space.n
+    #     c_len = config["c_len"]
+
+    #     train_dataset = TrajectoryDataset('dataset', c_len, state_dim, action_dim)
+    #     # train_dataset = Subset(train_dataset, [0])
+    #     model = DecisionTransformer(state_dim, action_dim, config["hidden_size"], c_len, 200, True, n_head=1, n_layer=3, device=config["device"])
+    #     trainer = Trainer(config, model, train_dataset, loss_fn=lambda a_hat, a: torch.mean((a_hat - a)**2))
+    #     trainer.train()
     wandb.login()
     env = gym.make('CartPole-v0')
+    print(torch.cuda.is_available())
 
-    with wandb.init(project='decision-transformer'):
-        wandb.config = {
-            "learning_rate": 1e-3,
-            "epochs": 100,
-            "batch_size": 64,
-            "hidden_size": 128,
-            "c_len": 50,
-            "device": "auto",
-        }
-        config = wandb.config
+    wandb.init(project='decision-transformer')
+    wandb.run.name = 'gpt2-colab-full-clen-20-lr-1e-4'
+    wandb.config = {
+        "learning_rate": 1e-3,
+        "epochs": 500,
+        "batch_size": 64,
+        "hidden_size": 64,
+        "c_len": 20,
+        "device": "auto",
+        "weight_decay": 1e-4,
+        "betas": (0.9, 0.999),
+        "activation_function": "relu",
+        'dropout': 0.1,
+        "warmup_steps": 10000,
+        "num_workers": 0
+    }
+    config = wandb.config
+    env = gym.make('CartPole-v0')
 
-        state_dim = env.observation_space.shape[0]
-        action_dim = env.action_space.n
-        c_len = config["c_len"]
-        model = DecisionTransformer(state_dim, action_dim, config["hidden_size"], c_len, 200, action_tanh=True, n_head=1, n_layer=3)
+    state_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.n
+    c_len = config["c_len"]
 
-        dataset = TrajectoryDataset('/dataset/traj_dataset.pkl', c_len, state_dim, action_dim)
-        trainer = Trainer(config, model, dataset, loss_fn=lambda a_hat, a: torch.mean((a_hat - a)**2))
-        trainer.train()
-
-        trainer.evaluate(env, dataset.state_mean, dataset.state_std)
+    train_dataset = TrajectoryDataset('dataset', c_len, state_dim, action_dim)
+    model = DecisionTransformer(state_dim, action_dim, config["hidden_size"], c_len, 200, True, n_head=1, n_layer=3, n_inner=4*config['hidden_size'],
+            activation_function=config['activation_function'],
+            n_positions=1024,
+            resid_pdrop=config['dropout'],
+            attn_pdrop=config['dropout'], device=config["device"])
+    trainer = Trainer(config, model, train_dataset, loss_fn=lambda a_hat, a: torch.mean((a_hat - a)**2))
+    trainer.train()
+    wandb.run.finish()
 
 if __name__ == '__main__':
     main()