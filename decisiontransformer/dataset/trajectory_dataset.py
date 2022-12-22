Fix action pred loss calculation and timesteps in get_action

decisiontransformer/dataset/trajectory_dataset.py
@@ -11,11 +11,12 @@ class TrajectoryDataset(Dataset):
         self.state_dim = state_dim
         self.action_dim = action_dim
         self.trajectories = []
-        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'trajectories.pkl')
+        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'traj_dataset_good.pkl')
         with open(path, 'rb') as f:
             self.trajectories = pickle.load(f)
         print('done')
         self.state_mean, self.state_std = self._compute_mean_std()
+        # self.save_mean_std()
 
     def __len__(self):
         return len(self.trajectories)
@@ -27,21 +28,24 @@ class TrajectoryDataset(Dataset):
         returns = self._compute_returns(0, traj["rewards"], mask, gamma=1)
 
         rand_start = random.randint(0, len(traj['states']) - self.c_len)
-        s = torch.stack(traj['states'][rand_start:rand_start+self.c_len]).reshape((-1, self.state_dim))
-        a = np.stack(traj['actions'][rand_start:rand_start+self.c_len]).reshape((-1, self.action_dim))
-        r = np.stack(returns[rand_start:rand_start+self.c_len]).reshape((-1, 1))
-        d = np.stack(traj['dones'][rand_start:rand_start+self.c_len])
+        seq_len = self.c_len
+        if rand_start == 0:
+            seq_len = random.randint(1, self.c_len)
+        s = torch.stack(traj['states'][rand_start:rand_start+seq_len]).reshape((-1, self.state_dim))
+        a = np.argmax(np.stack(traj['actions'][rand_start:rand_start+seq_len]).reshape((-1, self.action_dim)), axis=1)
+        r = np.stack(returns[rand_start:rand_start+seq_len]).reshape((-1, 1))
+        d = np.stack(traj['dones'][rand_start:rand_start+seq_len])
         timesteps = np.arange(rand_start, rand_start+len(s)) # TODO: check if padding cutoff is necessary and if rtg needs padding
 
         tlen = s.shape[0]
         s = s.cpu()
-        s = torch.concatenate((torch.zeros((self.c_len - tlen, self.state_dim)), s))
+        s = torch.concatenate((s, torch.zeros((self.c_len - tlen, self.state_dim))))
         s = (s - self.state_mean) / self.state_std
-        a = np.concatenate((np.ones((self.c_len - tlen, self.action_dim)) * -10., a))
-        r = np.concatenate((np.zeros((self.c_len - tlen, 1)), r))
-        d = np.concatenate((np.ones(self.c_len - tlen) * 2, d))
-        timesteps = np.concatenate((np.zeros(self.c_len - tlen), timesteps))
-        mask = np.concatenate((np.zeros(self.c_len - tlen), np.ones(tlen)))
+        a = np.concatenate((a, np.ones(self.c_len - tlen) * -10.))[:, np.newaxis]
+        r = np.concatenate((r, np.zeros((self.c_len - tlen, 1))))
+        d = np.concatenate((d, np.ones(self.c_len - tlen) * 2))
+        timesteps = np.concatenate((timesteps, np.zeros(self.c_len - tlen)))
+        mask = np.concatenate((np.ones(tlen), np.zeros(self.c_len - tlen)))
 
         return s, a, r, d, timesteps, mask
 
@@ -56,3 +60,7 @@ class TrajectoryDataset(Dataset):
     def _compute_mean_std(self):
         states = torch.cat([torch.concatenate(traj['states'], axis=0) for traj in self.trajectories], 0)
         return torch.mean(states, axis=0).cpu(), torch.std(states, axis=0).cpu() + 1e-6
+
+    def save_mean_std(self):
+        with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'trajectories_metadata.pkl'), 'wb') as f:
+            pickle.dump((self.state_mean, self.state_std), f)

decisiontransformer/decision_transformer.py
@@ -11,7 +11,6 @@ class DecisionTransformer(nn.Module):
         super().__init__()
         self.hidden_size = hidden_size
         transformer_config = transformers.GPT2Config(
-            vocab_size=act_dim,
             n_embd=hidden_size,
             block_size=context_length * 3,
             **kwargs,
@@ -25,7 +24,7 @@ class DecisionTransformer(nn.Module):
         # )
 
         self.state_dim = state_dim
-        self.act_dim = act_dim
+        self.act_dim = 1
         self.c_len = context_length
         if device == 'auto':
             self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -36,13 +35,11 @@ class DecisionTransformer(nn.Module):
 
         self.embed_timestep = nn.Embedding(max_ep_len, hidden_size)
         self.embed_return = nn.Linear(1, hidden_size)
-        self.embed_action = nn.Linear(act_dim, hidden_size)
+        self.embed_action = nn.Linear(1, hidden_size)
         self.embed_state = nn.Linear(state_dim, hidden_size)
 
         self.embed_ln = nn.LayerNorm(hidden_size)
-        self.predict_action = nn.Sequential(
-            *[nn.Linear(hidden_size, act_dim)] + ([nn.Tanh()] if action_tanh else [])
-            )
+        self.predict_action = nn.Sequential(nn.Linear(hidden_size, 1), nn.Sigmoid())
 
     def forward(self, states, actions, ret_to_go, timesteps, attn_mask=None):
         batch_size, seq_len = states.shape[0], states.shape[1]
@@ -66,19 +63,29 @@ class DecisionTransformer(nn.Module):
 
         stacked_attn_mask = torch.stack(
             (attn_mask, attn_mask, attn_mask), dim=1
-        ).permute(0, 2, 1).reshape(batch_size, 3*seq_len).to(device=self.device)
+        ).permute(0, 2, 1).reshape(batch_size, 1, 3*seq_len).to(device=self.device)
+        # stacked_attention_mask = torch.stack(
+        #     (attn_mask, attn_mask, attn_mask), dim=1
+        # ).permute(0, 2, 1).reshape(batch_size, 3*seq_len).to(device=self.device)
 
         logits = self.transformer(
             inputs_embeds=stacked_inputs,
             attention_mask=stacked_attn_mask,
         )
 
+        # transformer_outputs = self.transformer(
+        #     inputs_embeds=stacked_inputs,
+        #     attention_mask=stacked_attention_mask,
+        # )
+        # logits = transformer_outputs['last_hidden_state']
+
         logits = logits.reshape(batch_size, seq_len, 3, self.hidden_size).permute(0, 2, 1, 3)
-        action_hidden = logits[:, 2, :, :]
+        action_hidden = logits[:, 1, :, :]
 
         action_preds = self.predict_action(action_hidden)
+        # action_preds = self.predict_action(logits[:,1])
         return action_preds
-    
+
     def get_action(self, states, actions, returns_to_go, timesteps, **kwargs):
         states = states.reshape(1, -1, self.state_dim)
         actions = actions.reshape(1, -1, self.act_dim)
@@ -91,10 +98,11 @@ class DecisionTransformer(nn.Module):
         timesteps = timesteps[:,-self.c_len:]
 
         # pad all tokens to sequence length
-        attention_mask = torch.cat([torch.ones(states.shape[1]), torch.zeros(self.c_len-states.shape[1])])
-        attention_mask = attention_mask.to(dtype=torch.long, device=states.device).reshape(1, -1)
+        seq_len = states.shape[1]
+        attention_mask = torch.cat([torch.ones(seq_len), torch.zeros(self.c_len-seq_len)])
+        attention_mask = attention_mask.to(device=states.device).reshape(1, -1)
         states = torch.cat(
-            [states, torch.zeros((states.shape[0], self.c_len-states.shape[1], self.state_dim), device=states.device)],
+            [states, torch.zeros((states.shape[0], self.c_len-seq_len, self.state_dim), device=states.device)],
             dim=1).to(dtype=torch.float32)
         actions = torch.cat(
             [actions, torch.zeros((actions.shape[0], self.c_len - actions.shape[1], self.act_dim),
@@ -107,11 +115,17 @@ class DecisionTransformer(nn.Module):
             [timesteps, torch.zeros((timesteps.shape[0], self.c_len-timesteps.shape[1]), device=timesteps.device)],
             dim=1
         ).to(dtype=torch.long)
+        # print(attention_mask)
+        # print(states)
+        # print(actions)
+        # print(returns_to_go)
+        # print(timesteps)
 
         action_preds = self.forward(
             states, actions, returns_to_go, timesteps, attn_mask=attention_mask, **kwargs)
 
-        return action_preds[0, -1]
+        print(action_preds.shape)
+        return action_preds[0, seq_len - 1]
 
     def configure_optimizers(self, train_config):
         return self.transformer.configure_optimizers(train_config)

decisiontransformer/minGPT/transformer.py
@@ -27,7 +27,7 @@ class CausalSelfAttention(nn.Module):
         # mask = np.array([np.hstack((np.ones(3 * i - 1), np.zeros(actual_seq_len - (3 * i - 1)))) for i in range(1, config.block_size + 1)])
         # print(mask.shape)
         mask = torch.tril(torch.ones(actual_seq_len, actual_seq_len))
-        mask = torch.tensor(mask).view(1, 1, actual_seq_len, actual_seq_len)
+        mask = mask.clone().detach().view(1, 1, actual_seq_len, actual_seq_len)
         self.register_buffer("bias", mask)
         self.n_head = config.n_head
         self.n_embd = config.n_embd
@@ -43,16 +43,19 @@ class CausalSelfAttention(nn.Module):
 
         # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
         att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        # if mask is not None:
-        #     mask = 1 - mask
-        #     if len(mask.shape) != len(att.shape):
-        #         assert att[:, 0].shape == mask.shape
-        #         mask = mask.unsqueeze(dim=1)
-        #     else:
-        #         assert att.shape == mask.shape
-        #     att += -1e9 * mask
+        if mask is not None:
+            mask = 1 - mask
+            if len(mask.shape) != len(att.shape):
+                assert att[:, 0].shape == mask.shape
+                mask = mask.unsqueeze(dim=1)
+            else:
+                assert att.shape == mask.shape
+            att += -1e9 * mask
+        # print(att)
+        # print('====')
         att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
         att = F.softmax(att, dim=-1)
+        # print(att)
         att = self.attn_dropout(att)
         y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
         y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
@@ -220,10 +223,8 @@ class minGPT(nn.Module):
         """
         assert padding_mask.shape[0] == sequence.shape[0] and \
                                                 'batch size mismatch between input sequence and  padding_mask'
-        assert len(padding_mask.shape) == 2 and \
-                                                'Can only convert 2D position mask to 3D attention mask'
 
-        attention_mask = padding_mask[:, None, :].repeat(*(1, sequence.shape[1], 1))
+        attention_mask = padding_mask.transpose(-1, -2) @ padding_mask
         return attention_mask
 
     @torch.no_grad()

decisiontransformer/test.py
@@ -24,12 +24,39 @@ config = {
 state_dim = env.observation_space.shape[0]
 action_dim = env.action_space.n
 c_len = config["c_len"]
-model = DecisionTransformer(state_dim, action_dim, config["hidden_size"], c_len, 200, True, n_head=1, n_layer=3, n_inner=4*config['hidden_size'],
-        activation_function=config['activation_function'],
-        n_positions=1024,
-        resid_pdrop=config['dropout'],
-        attn_pdrop=config['dropout'], device=config["device"])
+# model = DecisionTransformer(state_dim, action_dim, config["hidden_size"], c_len, 200, True, n_head=1, n_layer=3, n_inner=4*config['hidden_size'],
+#         activation_function=config['activation_function'],
+#         n_positions=1024,
+#         resid_pdrop=config['dropout'],
+#         attn_pdrop=config['dropout'], device=config["device"]).cuda()
 
-# train_dataset = TrajectoryDataset(c_len, state_dim, action_dim)
+train_dataset = TrajectoryDataset(c_len, state_dim, action_dim)
+states, actions, returns, dones, timesteps, attn_mask = train_dataset[3]
+# states = states.to(dtype=torch.float).unsqueeze(0).cuda()
+# actions = torch.from_numpy(actions).to(dtype=torch.float).unsqueeze(0).cuda()
+# returns = torch.from_numpy(returns).to(dtype=torch.float).unsqueeze(0).cuda()
+# timesteps = torch.from_numpy(timesteps).to(dtype=torch.long).unsqueeze(0).cuda()
+# attn_mask = torch.from_numpy(attn_mask).unsqueeze(0).cuda()
+# stacked_attn_mask = torch.stack(
+#             (attn_mask, attn_mask, attn_mask), dim=1
+#         ).permute(0, 2, 1).reshape(attn_mask.shape[0], 1, 3*attn_mask.shape[1])
+# attention_mask = stacked_attn_mask.transpose(-1, -2) @ stacked_attn_mask
+# stuff = -1e9 * attention_mask
+# print(attn_mask)
+# action_preds = model(
+#                     states, actions, returns, timesteps=timesteps, attn_mask=attn_mask
+#                 )
 # print(len(train_dataset))
-torch.save(model.state_dict(), 'models/mingpt')
+# torch.save(model.state_dict(), 'models/mingpt')
+
+# attn_mask = torch.cat([torch.ones(1), torch.zeros(5-1)])
+# attn_mask2 = torch.cat([torch.ones(2), torch.zeros(5-2)])
+# attn_mask = torch.stack([attn_mask, attn_mask2], dim=0)
+# attn_mask = attn_mask.to(dtype=torch.long)
+# stacked_attn_mask = torch.stack(
+#             (attn_mask, attn_mask, attn_mask), dim=1
+#         ).permute(0, 2, 1).reshape(2, 1, 3*attn_mask.shape[1])
+# attention_mask = stacked_attn_mask.transpose(-1, -2) @ stacked_attn_mask
+
+# print(stacked_attn_mask)
+# print(attention_mask)

decisiontransformer/training.py
@@ -10,6 +10,8 @@ from decision_transformer import DecisionTransformer
 from dataset.trajectory_dataset import TrajectoryDataset
 import sys
 import os
+import pickle
+import random
 
 class Trainer:
     def __init__(self, config, model, train_dataset, loss_fn):
@@ -70,6 +72,8 @@ class Trainer:
                     states, actions, returns, timesteps=timesteps, attn_mask=attn_mask
                 )
 
+                action_preds = action_preds.reshape(-1, 1)[attn_mask.reshape(-1) > 0]
+                action_target = action_target.reshape(-1, 1)[attn_mask.reshape(-1) > 0]
                 loss = self.loss_fn(action_preds, action_target)
                 loss.backward()
                 self.optimizer.step()
@@ -81,7 +85,7 @@ class Trainer:
             print(np.mean(np.asarray(batch_losses)))
         
         self.train_losses = train_losses
-        torch.save(model.cpu().state_dict(), 'models/mingpt')
+        torch.save(model.cpu().state_dict(), 'models/gpt2_with_pad')
 
     @staticmethod
     def evaluate(model, env, state_mean, state_std, device, target_return=200):
@@ -90,12 +94,12 @@ class Trainer:
 
         model.eval()
         model.to(device=device)
-        state_dim, act_dim = env.observation_space.shape[0], env.action_space.n
+        state_dim, act_dim = env.observation_space.shape[0], 1
 
         state_mean = state_mean.to(device=device)
         state_std = state_std.to(device=device)
 
-        state = env.reset()
+        state, _info = env.reset()
 
         # we keep all the histories on the device
         # note that the latest action and reward will be "padding"
@@ -103,10 +107,10 @@ class Trainer:
         actions = torch.zeros((0, act_dim), device=device, dtype=torch.float32)
         ret_to_go = torch.zeros(1, device=device, dtype=torch.float32)
         ret_to_go[0] = target_return
-        timesteps = torch.from_numpy(np.arange(200)).to(device=device, dtype=torch.float32)
+        timesteps = torch.zeros(1, device=device, dtype=torch.float32)
 
         episode_return, episode_length = 0, 0
-        for t in timesteps:
+        for i in range(200):
 
             # add padding
             actions = torch.cat([actions, torch.zeros((1, act_dim), device=device)], dim=0)
@@ -118,14 +122,18 @@ class Trainer:
                 ret_to_go.to(dtype=torch.float32),
                 timesteps
             )
-            action = action.detach().cpu().numpy()
-            env_action = np.argmax(action)
+            action = action.detach().cpu()
+            env_action = (1 if action > 0.5 else 0)
+            print(action)
+            print(env_action)
             state, reward, done, _truncated, _info = env.step(env_action)
 
             cur_state = torch.from_numpy(state).to(device=device).reshape(1, state_dim)
             states = torch.cat([states, cur_state], dim=0)
             ret_to_go = torch.cat([ret_to_go, torch.zeros(1, device=device)])
             ret_to_go[-1] = ret_to_go[-2] - reward
+            actions[-1] = env_action
+            timesteps = torch.cat([timesteps, torch.tensor([i+1], device=device)])
 
             episode_return += reward
             episode_length += 1
@@ -138,12 +146,13 @@ class Trainer:
 
 def main():
     _, run_type = sys.argv
+    set_seed()
     env = gym.make('CartPole-v1')
     print(torch.cuda.is_available())
 
     train_config = {
         "learning_rate": 2e-4,
-        "epochs": 30,
+        "epochs": 15,
         "batch_size": 256,
         "weight_decay": 1e-4,
         "betas": (0.9, 0.999),
@@ -168,8 +177,8 @@ def main():
             resid_pdrop=model_config['dropout'],
             attn_pdrop=model_config['dropout'], device=model_config["device"])
 
-    if run_type == 'eval' and os.path.exists('models/mingpt'):
-        model.load_state_dict(torch.load('models/mingpt'))
+    if run_type == 'eval' and os.path.exists('models/gpt2_with_pad'):
+        model.load_state_dict(torch.load('models/gpt2_with_pad'))
     else:
         wandb.login()
         wandb.init(project='decision-transformer')
@@ -178,11 +187,24 @@ def main():
 
 
         train_dataset = TrajectoryDataset(c_len, state_dim, action_dim)
-        trainer = Trainer(full_config, model, train_dataset, loss_fn=lambda a_pred, a_target: torch.mean((a_pred - a_target) ** 2))
+        trainer = Trainer(full_config, model, train_dataset, loss_fn=nn.BCELoss())
         trainer.train()
         wandb.run.finish()
+    env.close()
 
-    Trainer.evaluate(model, env, train_dataset.state_mean, train_dataset.state_std, full_config['device'])
+    test_env = gym.make('CartPole-v1', render_mode='human')
+    with open('dataset/trajectories_metadata.pkl', 'rb') as f:
+        ds_mean, ds_std = pickle.load(f)
+
+    print(Trainer.evaluate(model, test_env, ds_mean, ds_std, full_config['device']))
+    test_env.close()
+
+def set_seed(seed: int = 42) -> None:
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    print(f"Random seed set as {seed}")
 
 if __name__ == '__main__':
     main()